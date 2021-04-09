625884b2322336bf106e3dcf29366fb5afca955a
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-04-09 17:18:15 -0400

    edit vendors table

--- server/db/schema/vendors.sql
+++ server/db/schema/vendors.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "vendors" (
  "id" SERIAL PRIMARY KEY NOT NULL,
 "vendor_name" VARCHAR(225) NOT NULL,
-"created_at" TIMESTAMP,
+"created_at" TIMESTAMP DEFAULT current_timestamp,
 "city_name" VARCHAR(225),
-"address"VARCHAR(225)
+"adress"VARCHAR(225)
 );
---
+++ server/db/seeds/02_vendors.sql
@@ -0,0 +1,3 @@
+INSERT INTO vendors(vendor_name, city_name, adress)
+VALUES
+('Dispatch', 'Little Italy',)
