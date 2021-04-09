a146705b129f5cf15371b2838d985fb7dcbd20d1
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-04-09 17:01:33 -0400

    seeds 01

---
+++ server/db/seeds/01_items.sql
@@ -0,0 +1,14 @@
+INSERT INTO items(item_name, item_type, total_quantity, cost)
+VALUES
+('Brazilian Coffee', 'Robusta', 2500, 150),
+('Jamaican Coffee', 'Jamaican Blue Mountain', 4000, 180),
+('Columbian Coffee', 'Arabica', 3000, 200),
+('Canadian Coffee', 'Liberica', 3000, 400),
+('Columbian Coffee', 'Bourbon', 3000, 140),
+('Italian Coffee', 'Catimor', 3000, 190),
+('Ethiopian Coffee', 'Excelsa', 2500, 250),
+('Turkish Coffee', 'Arabica', 1900, 300),
+('Hawaiian Coffee', 'Kona Typica', 3300, 270),
+('Columbian Coffee', 'Maragogype', 3000, 200),
+('Uganda Coffee', 'Maracatu', 2225, 200),
+('Vietnamese Coffee', 'Geisha', 1750, 200)
