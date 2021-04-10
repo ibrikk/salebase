83359d6196a28480bd4c5426b4bc189eb558c08b
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-04-09 22:57:09 -0400

    fix connection from be to fe, setState to api data

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+// import { HashRouter, Route, Switch } from 'react-router-dom';
 import './scss/style.scss';
+import axios from 'axios';
 
-const loading = (
-  <div className="pt-3 text-center">
-    <div className="sk-spinner sk-spinner-pulse"></div>
-  </div>
-)
+// const loading = (
+//   <div className="pt-3 text-center">
+//     <div className="sk-spinner sk-spinner-pulse"></div>
+//   </div>
+// )
 
-// Containers
-const TheLayout = React.lazy(() => import('./containers/TheLayout'));
+// // Containers
+// const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
-// Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+// // Pages
+// const Login = React.lazy(() => import('./views/pages/login/Login'));
+// const Register = React.lazy(() => import('./views/pages/register/Register'));
+// const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
+// const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' }
+  };
+
+componentDidMount() {
+axios.get(`http://localhost:3001`)
+        .then(res => {
+          console.log(res.data)
+          const obj = res.data;
+          this.setState({obj})
+        });
+
+  }
+
 
   render() {
     return (
-      <HashRouter>
-          <React.Suspense fallback={loading}>
-            <Switch>
-              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
-              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
-              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
-            </Switch>
-          </React.Suspense>
-      </HashRouter>
+      <p>{this.state.obj}</p>
+      // <HashRouter>
+      //     <React.Suspense fallback={loading}>
+      //       <Switch>
+      //         <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+      //         <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+      //         <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+      //         <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+      //         <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
+      //       </Switch>
+      //     </React.Suspense>
+      // </HashRouter>
     );
   }
 }
