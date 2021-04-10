a4221aeb6a82c9cbc071665677c4cd68fc3aca54
OdeliaFink <odelia.finkel@gmail.com>
2021-04-10 15:51:25 +0000

    Install PG, Creating client connection with DB

--- server/app.js
+++ server/app.js
@@ -1,45 +1,61 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const db = require("./db");
+const PORT = process.env.PORT || 3001;
+const dbHelpers = require("./helpers/dbHelpers")(db);
 
-const indexRouter = require('./routes/index');
-const itemsRouter = require('./routes/items');
-const itemsAssignRouter = require('./routes/items-assign');
-const biRouter = require('./routes/bi');
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("./lib/db");
+console.log("db connection test", dbParams);
+const db = new Pool(dbParams);
+db.connect((err) => console.log("connected", err));
+
+const indexRouter = require("./routes/index");
+const itemsRouter = require("./routes/items");
+const itemsAssignRouter = require("./routes/items-assign");
+const biRouter = require("./routes/bi");
 
 const app = express();
+// const port = normalizePort(process.env.PORT || "3001");
+app.set("port", port);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/items', itemsRouter);
-app.use('/items-assign', itemsAssignRouter);
-app.use('/bi', biRouter);
+app.use("/", indexRouter(dbHelpers));
+app.use("/items", itemsRouter(dbHelpers));
+app.use("/items-assign", itemsAssignRouter(dbHelpers));
+app.use("/bi", biRouter(dbHelpers));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
 
 module.exports = app;
--- server/routes/items.js
+++ server/routes/items.js
@@ -1,25 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-/* GET items listing. */
-router.get('/', function(req, res, next) {
-  res.send('This is the Items route!');
+/* GET items listing. Homepage */
+router.get("/", function (req, res, next) {
+  res.send("This is the Items route!");
 });
 
 /* GET items listing. */
-router.post('/', function(req, res, next) {
-  res.send('This post items route!');
+router.post("/", function (req, res, next) {
+  res.send("This post items route!");
 });
 
-router.put('/:id', function(req, res, next) {
-  res.send('This put items route!');
+router.put("/:id", function (req, res, next) {
+  res.send("This put items route!");
 });
 
-router.delete('/:id', function(req, res, next) {
-  res.send('This delete items route!');
+router.delete("/:id", function (req, res, next) {
+  res.send("This delete items route!");
 });
-
-
 
 module.exports = router;
-
