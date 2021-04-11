ff4d962c85ef756ce0ada29b67ff21a543db60aa
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-04-11 17:16:54 -0400

    fix footer, build hrefs

--- client/src/containers/TheFooter.js
+++ client/src/containers/TheFooter.js
@@ -5,12 +5,12 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a href="../views/AboutUs/AboutUs.js#AboutUs" target="_blank" rel="noopener noreferrer">Salebase</a>
+        <span className="ml-1">&copy; 2021 Lighthouse Labs Grads</span>
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI for React</a>
+        <a href='../views/AboutUs/AboutUs.js#AboutUs'>Salebase</a>
       </div>
     </CFooter>
   )
