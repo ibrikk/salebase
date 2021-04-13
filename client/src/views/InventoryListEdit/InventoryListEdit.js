137acf6dd65f2564af246210c16f76a4f05e08b8
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-04-13 14:59:17 -0400

    add forms, save button

--- client/src/views/InventoryListEdit/InventoryListEdit.js
+++ client/src/views/InventoryListEdit/InventoryListEdit.js
@@ -2,8 +2,8 @@ import React, { lazy, useState, useEffect } from "react";
 import axios from "axios";
 
 import {
-  CCard,
-  CCardBody,
+  // CCard,
+  // CCardBody,
   CRow,
   // CNavbar,
   // CToggler,
@@ -17,8 +17,15 @@ import {
   // CDropdownMenu,
   // CDropdownItem,
   // CNavbarBrand,
-  CBadge,
-  CDataTable,
+  // CBadge,
+  // CDataTable,
+  CCol,
+  CContainer,
+  CInput,
+  CLabel,
+  CForm,
+  CFormGroup,
+  // CFormText,
 
   // CButtonGroup,
   // CCardFooter,
@@ -29,13 +36,66 @@ import {
 } from "@coreui/react";
 
 const InventoryListEdit = () => {
-
-
   return (
     <div>
       <h1>InventoryListEdit</h1>
+      <CContainer fluid>
+        <CRow>
+          <CCol sm="12">
+            <CForm action="" method="post">
+              <CFormGroup>
+                <CLabel htmlFor="nf-email">Item Name</CLabel>
+                <CInput
+                  type="text"
+                  id="nf-email"
+                  name="nf-email"
+                  placeholder="Enter Item Name"
+                  autoComplete="email"
+                />
+              </CFormGroup>
+              <CFormGroup>
+                <CLabel htmlFor="nf-password">Item Type</CLabel>
+                <CInput
+                  type="text"
+                  id="nf-password"
+                  name="nf-password"
+                  placeholder="Enter Item Type"
+                  autoComplete="current-password"
+                />
+              </CFormGroup>
+              <CFormGroup>
+                <CLabel htmlFor="nf-password">Quantity</CLabel>
+                <CInput
+                  type="text"
+                  id="nf-password"
+                  name="nf-password"
+                  placeholder="Enter Quantity"
+                  autoComplete="current-password"
+                />
+              </CFormGroup>
+              <CFormGroup>
+                <CLabel htmlFor="nf-password">Cost/lbs</CLabel>
+                <CInput
+                  type="text"
+                  id="nf-password"
+                  name="nf-password"
+                  placeholder="Enter Cost per Pound"
+                  autoComplete="current-password"
+                />
+              </CFormGroup>
+            </CForm>
+          </CCol>
+        </CRow>
+        <CButton
+          color="primary"
+          style={{ float: "right" }}
+         /* onClick={(event) => (Post to DB  )}  */
+        >
+          <strong>SAVE</strong>
+        </CButton>
+      </CContainer>
     </div>
-  )
-}
+  );
+};
 
 export default InventoryListEdit;
