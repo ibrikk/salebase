ee00800e45eda606fca1f25b363d7b14a70f15bd
Dasha <dasha.kotova.mail@gmail.com>
2021-04-14 13:42:19 -0400

    add inventory assignment

--- client/src/views/InventoryAssignment/InventoryAssignment.js
+++ client/src/views/InventoryAssignment/InventoryAssignment.js
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from "react";
-import {CChart} from '@coreui/react-chartjs';
+import React, { useState, useEffect} from "react";
 import {
   CCard,
   CCardBody,
   CRow,
-  // CNavbar,
-  // CToggler,
-  // CCollapse,
-  // CNavbarNav,
-  // CForm,
-  // CInput,
+  CNavbar,
+  CToggler,
+  CCollapse,
+  CNavbarNav,
+  CForm,
+  CInput,
   CButton,
-  // CDropdown,
-  // CDropdownToggle,
-  // CDropdownMenu,
-  // CDropdownItem,
-  // CNavbarBrand,
+  CDropdown,
+  CDropdownToggle,
+  CDropdownMenu,
+  CDropdownItem,
+  CNavbarBrand,
   CBadge,
   CDataTable,
-  CProgress,
-  CProgressBar,
 
   // CButtonGroup,
   // CCardFooter,
@@ -28,74 +25,48 @@ import {
   // CProgress,
   // CCallout
 } from "@coreui/react";
-
-
-
-import axios from "axios";
+import axios from 'axios';
 
 const InventoryAssignment = () => {
 
-  const doughnut = {
-    labels: [
-      'Red',
-      'Green',
-      'Yellow',
-    ],
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: [
-          '#FF6384',
-          '#36A2EB',
-          '#FFCE56',
-        ],
-        hoverBackgroundColor: [
-          '#FF6384',
-          '#36A2EB',
-          '#FFCE56',
-        ],
-      }],
-    };
-
-
-  let counter = 86;
-
-  //const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/items-assign").then((res) => {
+    axios.get("http://localhost:3002/items").then((res) => {
       console.log(res.data.items);
       setData(res.data.items);
     });
   }, []);
 
-  /* const mapData = data => {
-    let arr = [];
-    for (const )
-  } */
   const [details, setDetails] = useState([]);
   // const [items, setItems] = useState(usersData)
 
-  // const toggleDetails = (index) => {
-  //   const position = details.indexOf(index);
-  //   let newDetails = details.slice();
-  //   if (position !== -1) {
-  //     newDetails.splice(position, 1);
-  //   } else {
-  //     newDetails = [...details, index];
-  //   }
-  //   setDetails(newDetails);
-  // };
+  const toggleDetails = (index) => {
+    const position = details.indexOf(index);
+    let newDetails = details.slice();
+    if (position !== -1) {
+      newDetails.splice(position, 1);
+    } else {
+      newDetails = [...details, index];
+    }
+    setDetails(newDetails);
+  };
 
   const fields = [
-    { key: "id", _style: { width: "40%" } },
+    { key: "item_name", _style: { width: "40%" } },
 
-    { key: "assigned_quantity", _style: { width: "20%" } },
+    { key: "item_type", _style: { width: "20%" } },
 
-
+    { key: "cost", label: "Cost/lbs", _style: { width: "20%" } },
     { key: "total_quantity", _style: { width: "20%" } },
-
+    {
+      key: "show_details",
+      label: "Edit",
+      _style: { width: "1%" },
+      sorter: false,
+      filter: false,
+    },
   ];
 
   const getBadge = (cost) => {
@@ -113,49 +84,50 @@ const InventoryAssignment = () => {
     }
   };
 
+
+
   return (
     <>
-    <div className="col-md-6">
-      <h4>Doughnut</h4>
-        <div className="chart-wrapper">
-          <CChart type="doughnut" datasets={doughnut.datasets} labels={doughnut.labels}/>
-        </div>
-        <hr />
-      </div>
-
-
-      <div className="col-md-6"></div>
-      <div className={"mt-2"}>
-        <CProgress className="mb-1 bg-white">
-          <CProgressBar
-            showValue
-            value={counter * (6 / 10) + 3}
-            color="success"
-            height="30px"
-          />
-          <CProgressBar
-            showValue
-            value={counter * (2.5 / 10) + 3}
-            color="warning"
-          />
-          <CProgressBar
-            showValue
-            value={counter * (1.5 / 10) + 3}
-            color="danger"
-          />
-        </CProgress>
-        <hr/>
-      </div>
-
-      <CButton
-        color="success"
-        onClick={(event) => (window.location.href = "/#/InventoryListEdit")}
-      >
-        <strong>ADD+</strong>
-      </CButton>
-
       <CCard>
         <CCardBody>
+          <div>
+            <CNavbar expandable="sm" color="info">
+              <CToggler inNavbar onClick={() => setIsOpen(!isOpen)} />
+              <CNavbarBrand>Inventory List</CNavbarBrand>
+              <CCollapse show={isOpen} navbar>
+                <CNavbarNav className="ml-auto">
+                  <CForm inline>
+                    <CInput
+                      className="mr-sm-2"
+                      placeholder="Search"
+                      size="sm"
+                    />
+                    <CButton
+                      color="light"
+                      className="my-2 my-sm-0"
+                      type="submit"
+                    >
+                      Search
+                    </CButton>
+                  </CForm>
+                  <CDropdown inNav>
+                    <CDropdownToggle color="primary">Price</CDropdownToggle>
+                    <CDropdownMenu>
+                      <CDropdownItem>Low-High</CDropdownItem>
+                      <CDropdownItem>High-Low</CDropdownItem>
+                    </CDropdownMenu>
+                  </CDropdown>
+                  <CDropdown inNav>
+                    <CDropdownToggle color="primary">Quantity</CDropdownToggle>
+                    <CDropdownMenu>
+                      <CDropdownItem>Low-High</CDropdownItem>
+                      <CDropdownItem>High-Low</CDropdownItem>
+                    </CDropdownMenu>
+                  </CDropdown>
+                </CNavbarNav>
+              </CCollapse>
+            </CNavbar>
+          </div>
           <CDataTable
             items={data}
             fields={fields}
@@ -173,7 +145,6 @@ const InventoryAssignment = () => {
                   <CBadge color={getBadge(item.cost)}>{item.cost}</CBadge>
                 </td>
               ),
-
               show_details: (item, index) => {
                 return (
                   <td className="py-2">
@@ -182,11 +153,11 @@ const InventoryAssignment = () => {
                       variant="outline"
                       shape="square"
                       size="sm"
-                      onClick={(event) =>
-                        (window.location.href = "/#/InventoryListEdit")
-                      }
+                      onClick={() => {
+                        toggleDetails(index);
+                      }}
                     >
-                      {details.includes(index) ? "Hide" : "Edit"}
+                      {details.includes(index) ? "Hide" : "Show"}
                     </CButton>
                   </td>
                 );
