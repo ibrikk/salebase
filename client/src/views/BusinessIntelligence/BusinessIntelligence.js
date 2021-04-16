6b899aec564adffee6ca35ec923c739e04d78200
Dasha <dasha.kotova.mail@gmail.com>
2021-04-16 10:47:26 -0400

    add business intelligence graphs

--- client/src/views/BusinessIntelligence/BusinessIntelligence.js
+++ client/src/views/BusinessIntelligence/BusinessIntelligence.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../../scss/style.scss";
+import '../../scss/style.scss';
 import {
   CCard,
   CCardBody,
@@ -17,54 +17,48 @@ import { CChart } from "@coreui/react-chartjs";
 import axios from "axios";
 
 const BusinessIntelligence = () => {
+
   const [products, setProducts] = useState([]);
 
-  const topItemsMap = (products) => {
-    let arr = [];
+
+
+    const topItemsMap = products => {
+      let arr = [];
     for (const item of products) {
-      arr.push(item.item_name);
-    }
-    return arr;
-  };
+        arr.push(item.item_name)
 
-  const topSumMap = (products) => {
+    }
+      return arr;
+    }
+
+  const topSumMap = products => {
     let array = [];
-    for (const sum of products) {
-      array.push(sum.sum);
-    }
-    return array;
-  };
+  for (const sum of products) {
+    array.push(sum.sum)
+  }
+  return array;
+  }
 
-  useEffect(() => {
-    axios.get("http://localhost:3002/bi").then((res) => {
-      // console.log("response", res.data.items);
-      setProducts(res.data.items);
-    });
-    axios.get("http://localhost:3002/bi/neighbourhood").then((res) => {
-      console.log("response", res.data.items);
-      //setProducts(res.data.items);
-    });
-  }, []);
+
+    useEffect(() => {
+      axios.get("http://localhost:3002/bi").then((res) => {
+        // console.log("response", res.data.items);
+        setProducts(res.data.items);
+      });
+      axios.get("http://localhost:3002/bi/neighbourhood").then((res) => {
+        console.log("response", res.data.items);
+        //setProducts(res.data.items);
+      });
+    }, []);
 
   const pie = {
     labels: ["Ahuntsic", "Rosemont", "Villeray", "Little Italy", "NDG"],
     datasets: [
       {
+        label: "Top Neighbourhoods",
         data: [789, 468, 382, 344, 322],
-        backgroundColor: [
-          "#E64040",
-          "#E67d40",
-          "#E6d540",
-          "#82E640",
-          "#40C2E6",
-        ],
-        hoverBackgroundColor: [
-          "#E64040",
-          "#E67d40",
-          "#E6d540",
-          "#82E640",
-          "#40C2E6",
-        ],
+        backgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
+        hoverBackgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6"],
         label: "Top Neighborhood",
       },
     ],
@@ -74,33 +68,37 @@ const BusinessIntelligence = () => {
     labels: topItemsMap(products),
     datasets: [
       {
-        label: "Top Selling Product",
-        backgroundColor: [
-          "#E64040",
-          "#E67d40",
-          "#E6d540",
-          "#82E640",
-          "#40C2E6",
-        ],
+        label: "Top Selling Products",
+        backgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
         borderColor: ["#EBf5f7"],
         borderWidth: 1,
-        hoverBackgroundColor: [
-          "#E64040",
-          "#E67d40",
-          "#E6d540",
-          "#82E640",
-          "#40C2E6",
-        ],
+        hoverBackgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
         hoverBorderColor: "rgba(255,99,132,1)",
         data: topSumMap(products),
       },
     ],
   };
+
+  const bar2 = {
+    labels: ['Cafe Vito', 'Cafe Pista', 'Paolino', 'Cafe Noir', 'Le Bruloir',],
+    datasets: [
+      {
+        label: "Top Vendors",
+        backgroundColor: ["#82E640", "#0C6E0E", "#40C2E6", "#406fE6", "#7a1091"],
+        borderColor: ["#EBf5f7"],
+        borderWidth: 1,
+        hoverBackgroundColor: ["#E67d40", "#E6d540", "#82E640", "#0C6E0E", "#40C2E6", "#406fE6", "#7a1091"],
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: [23, 26, 18, 53, 8, 2, 3],
+      },
+    ],
+  };
+
   const line = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: "Top Products By Month",
+        label: "Sales by Month",
         fill: false,
         lineTension: 0.1,
         backgroundColor: "#E64040",
@@ -114,68 +112,70 @@ const BusinessIntelligence = () => {
         pointBorderWidth: 1,
         pointHoverRadius: 5,
         pointHoverBackgroundColor: "#E64040",
-        pointHoverBorderColor: [
-          "#E64040",
-          "#E67d40",
-          "#E6d540",
-          "#82E640",
-          "#40C2E6",
-        ],
+        pointHoverBorderColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40],
+        data: [23, 26, 18, 53, 8, 2, 3],
       },
     ],
   };
 
-  const [itemName, setItemName] = useState("");
-
-  useEffect(() => {
-    axios.get("http://localhost:3002/bi").then((res) => {
-      console.log("response", res.data.items);
-      setItemName(res.data.items);
-      console.log("itemName", itemName);
-    });
-  }, []);
+  //console.log("itemName", itemName);
+// console.log(topItemsMap(products))
 
   return (
     <>
-      <CCard>
-        <CCardBody>
-          <div className="charts">
-            <CRow>
-              <div className="col-md-8">
-                <div className="chart-wrapper">
-                  <CChart
-                    type="line"
-                    datasets={line.datasets}
-                    labels={line.labels}
-                  />
-                </div>
-              </div>
+      <CCard >
+        <CCardBody >
+        <div className='charts'>
 
-              <div className="col-lg-8">
-                <div className="chart-wrapper">
-                  <CChart
-                    type="pie"
-                    datasets={pie.datasets}
-                    labels={pie.labels}
-                  />
-                </div>
-              </div>
+          <CRow >
 
-              <div className="col-lg-8">
-                <div className="bar-wrapper">
-                  <CChart
-                    type="bar"
-                    datasets={bar.datasets}
-                    labels={bar.labels}
-                    //options={options}
-                  />
-                </div>
+            <div className="col-md-8">
+              <div className="chart-wrapper">
+                <CChart
+                  type="line"
+                  datasets={line.datasets}
+                  labels={line.labels}
+                />
               </div>
-            </CRow>
+            </div>
+
+            <div className="col-lg-8">
+              <div className="chart-wrapper">
+                <CChart
+                  type="pie"
+                  datasets={pie.datasets}
+                  labels={pie.labels}
+                />
+
+              </div>
+            </div>
+
+            <div className="col-lg-8">
+              <div className="bar-wrapper">
+                <CChart
+                  type="bar"
+                  datasets={bar.datasets}
+                  labels={bar.labels}
+                  //options={options}
+                />
+              </div>
+            </div>
+
+            <div className="col-lg-8">
+              <div className="bar-wrapper">
+                <CChart
+                  type="bar"
+                  datasets={bar2.datasets}
+                  labels={bar2.labels}
+                  //options={options}
+                />
+              </div>
+            </div>
+
+          </CRow>
           </div>
         </CCardBody>
       </CCard>
