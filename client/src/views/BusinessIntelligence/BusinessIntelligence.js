c85ea0b3cb8d1b7df39c544d01d3d3aa5ff7d3bf
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-04-23 16:25:33 -0400

    add comments, fix lint

--- client/src/views/BusinessIntelligence/BusinessIntelligence.js
+++ client/src/views/BusinessIntelligence/BusinessIntelligence.js
@@ -1,65 +1,60 @@
 import React, { useEffect, useState } from "react";
-import '../../scss/style.scss';
-import {
-  CCard,
-  CCardBody,
-  CRow,
-  // CBadge,
-  // CButton,
-  // CButtonGroup,
-  // CCardFooter,
-  CCardHeader,
-  // CProgress,
-  // CCallout
-} from "@coreui/react";
+import "../../scss/style.scss";
+import { CCard, CCardBody, CCardHeader } from "@coreui/react";
 
 import { CChart } from "@coreui/react-chartjs";
 import axios from "axios";
 
 const BusinessIntelligence = () => {
-
   const [products, setProducts] = useState([]);
 
-
-
-    const topItemsMap = products => {
-      let arr = [];
+  // Display top selling items - mapper function - data coming from Backend
+  const topItemsMap = (products) => {
+    let arr = [];
     for (const item of products) {
-        arr.push(item.item_name)
-
+      arr.push(item.item_name);
     }
-      return arr;
-    }
+    return arr;
+  };
 
-  const topSumMap = products => {
+  // Display top selling items quantity - mapper function - data coming from Backend
+
+  const topSumMap = (products) => {
     let array = [];
-  for (const sum of products) {
-    array.push(sum.sum)
-  }
-  return array;
-  }
+    for (const sum of products) {
+      array.push(sum.sum);
+    }
+    return array;
+  };
 
-
-    useEffect(() => {
-      axios.get("http://localhost:3002/bi").then((res) => {
-        // console.log("response", res.data.items);
-        setProducts(res.data.items);
-      });
-      axios.get("http://localhost:3002/bi/neighbourhood").then((res) => {
-        console.log("response", res.data.items);
-        //setProducts(res.data.items);
-      });
-    }, []);
+  useEffect(() => {
+    axios.get("http://localhost:3002/bi").then((res) => {
+      setProducts(res.data.items);
+    });
+    axios.get("http://localhost:3002/bi/neighbourhood").then((res) => {});
+  }, []);
 
   const pie = {
+    // Hardcoding labels for the demo (time constraint), will implement the feature after the demo-day
     labels: ["Ahuntsic", "Rosemont", "Villeray", "Little Italy", "NDG"],
     datasets: [
       {
         label: "Top Neighbourhoods",
         data: [789, 468, 382, 344, 322],
-        backgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
-        hoverBackgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6"],
-        label: "Top Neighborhood",
+        backgroundColor: [
+          "#E64040",
+          "#E67d40",
+          "#E6d540",
+          "#82E640",
+          "#40C2E6",
+        ],
+        hoverBackgroundColor: [
+          "#E64040",
+          "#E67d40",
+          "#E6d540",
+          "#82E640",
+          "#40C2E6",
+        ],
       },
     ],
   };
@@ -69,25 +64,52 @@ const BusinessIntelligence = () => {
     datasets: [
       {
         label: "Top Selling Products",
-        backgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
+        backgroundColor: [
+          "#E64040",
+          "#E67d40",
+          "#E6d540",
+          "#82E640",
+          "#40C2E6",
+        ],
         borderColor: ["#EBf5f7"],
         borderWidth: 1,
-        hoverBackgroundColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
+        hoverBackgroundColor: [
+          "#E64040",
+          "#E67d40",
+          "#E6d540",
+          "#82E640",
+          "#40C2E6",
+        ],
         hoverBorderColor: "rgba(255,99,132,1)",
         data: topSumMap(products),
       },
     ],
   };
 
+  // Hardcoding labels for the demo (time constraint), will implement the feature after the demo-day
   const bar2 = {
-    labels: ['Cafe Vito', 'Cafe Pista', 'Paolino', 'Cafe Noir', 'Le Bruloir',],
+    labels: ["Cafe Vito", "Cafe Pista", "Paolino", "Cafe Noir", "Le Bruloir"],
     datasets: [
       {
         label: "Top Vendors",
-        backgroundColor: ["#82E640", "#0C6E0E", "#40C2E6", "#406fE6", "#7a1091"],
+        backgroundColor: [
+          "#82E640",
+          "#0C6E0E",
+          "#40C2E6",
+          "#406fE6",
+          "#7a1091",
+        ],
         borderColor: ["#EBf5f7"],
         borderWidth: 1,
-        hoverBackgroundColor: ["#E67d40", "#E6d540", "#82E640", "#0C6E0E", "#40C2E6", "#406fE6", "#7a1091"],
+        hoverBackgroundColor: [
+          "#E67d40",
+          "#E6d540",
+          "#82E640",
+          "#0C6E0E",
+          "#40C2E6",
+          "#406fE6",
+          "#7a1091",
+        ],
         hoverBorderColor: "rgba(255,99,132,1)",
         data: [23, 26, 18, 53, 8, 2, 3],
       },
@@ -112,7 +134,13 @@ const BusinessIntelligence = () => {
         pointBorderWidth: 1,
         pointHoverRadius: 5,
         pointHoverBackgroundColor: "#E64040",
-        pointHoverBorderColor: ["#E64040", "#E67d40", "#E6d540", "#82E640", "#40C2E6",],
+        pointHoverBorderColor: [
+          "#E64040",
+          "#E67d40",
+          "#E6d540",
+          "#82E640",
+          "#40C2E6",
+        ],
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
@@ -121,15 +149,12 @@ const BusinessIntelligence = () => {
     ],
   };
 
-  //console.log("itemName", itemName);
-// console.log(topItemsMap(products))
-
   return (
     <>
-     <div className="row">
+      <div className="row">
         <div className="col-6">
           <CCard>
-          <CCardHeader>Sales by Month</CCardHeader>
+            <CCardHeader>Sales by Month</CCardHeader>
             <CCardBody>
               <div className="chart-wrapper">
                 <CChart
@@ -143,7 +168,7 @@ const BusinessIntelligence = () => {
         </div>
         <div className="col-6">
           <CCard>
-          <CCardHeader>Top Neighbourhoods</CCardHeader>
+            <CCardHeader>Top Neighbourhoods</CCardHeader>
             <CCardBody>
               <div className="chart-wrapper">
                 <CChart
@@ -159,7 +184,7 @@ const BusinessIntelligence = () => {
       <div className="row">
         <div className="col-6">
           <CCard>
-          <CCardHeader>Top Selling Products</CCardHeader>
+            <CCardHeader>Top Selling Products</CCardHeader>
             <CCardBody>
               <div className="bar-wrapper">
                 <CChart
@@ -173,11 +198,10 @@ const BusinessIntelligence = () => {
           </CCard>
         </div>
 
-
-      {/* <div className="row"> */}
-         <div className="col-6">
+        {/* <div className="row"> */}
+        <div className="col-6">
           <CCard>
-          <CCardHeader>Top Vendors</CCardHeader>
+            <CCardHeader>Top Vendors</CCardHeader>
             <CCardBody>
               <div className="bar-wrapper">
                 <CChart
@@ -189,8 +213,8 @@ const BusinessIntelligence = () => {
               </div>{" "}
             </CCardBody>
           </CCard>
-         </div>
-         </div>
+        </div>
+      </div>
       {/* </div> */}
     </>
   );
