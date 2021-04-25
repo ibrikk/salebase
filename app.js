6672631b522a26be359b09011387499185863018
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-04-25 14:28:03 -0400

    edit express static

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("/", indexRouter(dbHelpers));
 app.use("/items", itemsRouter(dbHelpers));
